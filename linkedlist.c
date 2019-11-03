a066ff831db4ca46db22cec967b142bbe6f8171a
Steve Wang <swang01@stuy.edu>
2019-11-03 20:10:29 +0000

    updated

--- linkedlist.c
+++ linkedlist.c
@@ -6,7 +6,7 @@
 void print_list(struct song_node * head){
   printf("[ ");
   while (head != NULL){
-    printf("%d %d ",head->artist, head->name);
+    printf("%s %s | ",head->artist, head->name);
     head = head->next;
   }
   printf("]\n");
@@ -15,11 +15,9 @@ void print_list(struct song_node * head){
 struct song_node * insert_front(struct song_node * head, char *name, char *artist){
   struct song_node * new;
   new = malloc(sizeof(struct song_node));
-  new->next = head;
-  new->name = calloc(strlen(name), sizeof(char));
   strcpy(new->name, name);
-  new->artist = calloc(strlen(artist), sizeof(char));
   strcpy(new->artist, artist);
+  new->next = head;
   return new;
 }
 
@@ -52,14 +50,36 @@ struct song_node * remove_node(struct song_node *front, char *name){
   return front;
 }
 
-/*struct node * insert_order(struct node *front, char *name, char *artist){
-  struct node * new = front;
-  struct node *after = front->next;
-  new = (struct node *)malloc(sizeof(struct node));
-  new->name= name;
-  new->artist = artist;
-  while (strcmp(new->name, after->name)<= 0){
-    new = new->next;
-    after = new->next;
+struct song_node * find_song(struct song_node *n, char *name, char *artist){
+  while (n != NULL){
+    if((strcmp(n->artist, artist) == 0) && (strcmp(n->name,name)) == 0){
+      return n;
+    }
+    n = n->next;
+  }
+  return NULL;
+}
+
+/*struct song_node * insert_order(struct song_node *n, char *name, char *artist){
+  if (n == NULL){
+    return insert_front(n, name, artist);
+  }
+  struct song_node *new;
+  struct song_node *current = n;
+  struct song_node *after = n->next;
+  new = (struct song_node *)malloc(sizeof(struct song_node));
+  strcpy(new->name, name);
+  strcpy(new->artist, artist);
+  while ((after != NULL) && (so){
+    if (strcmp(current->artist,artist) > 0){
+      if (strcmp(current->name, name) > 0){
+        new->next = after;
+        current->next = new;
+      }
+      current = after;
+      after = after->next;
+    }
+    current->next = new;
+    return new;
   }
 } */
