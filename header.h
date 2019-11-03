8fc5f64aab4a47a279662a5b0182dfadcdbbdf6d
Steve Wang <swang01@stuy.edu>
2019-11-03 20:14:13 +0000

    updated

--- header.h
+++ header.h
@@ -4,3 +4,4 @@ void print_list(struct song_node * head);
 struct song_node * insert_front(struct song_node * head, char *name, char *artist);
 struct song_node * free_list(struct song_node * head);
 struct song_node * remove_node(struct song_node *front, char *name);
+struct song_node * find_song(struct song_node *n, char *name, char *artist);
